4fd752cf1f8f8b9225168dbbdf8e6ef47052d870
Sebexe <sebyex304@gmail.com>
2025-09-13 16:43:46 -0300

    Agregada la configuracion de cors para el frontend (#12)

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.db.config import create_db_and_tables, seed_initial_data
 from app.routers import actores_router
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.recommendations import router as recommendations_router
 from strawberry.fastapi import GraphQLRouter
 from app.graphql.schema import schema
@@ -26,6 +26,20 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(actores_router)
 app.include_router(recommendations_router)
 app.include_router(graphql_app, prefix="/graphql")
+
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
