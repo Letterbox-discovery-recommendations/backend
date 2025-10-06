fcba9078981212c5a5e9f099ea90156378c9695b
Sebexe <sebyex304@gmail.com>
2025-10-06 16:26:10 -0300

    Agregados nuevos cambios para integrarlo con el core

--- app/main.py
+++ app/main.py
@@ -1,21 +1,31 @@
+import threading
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends
-from fastapi.security import HTTPBearer
-
+import logging
+from app.consumer import start_consuming
 from app.db.seed import create_db_and_tables, seed_initial_data
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import recommendations_router, rankings_router
 from strawberry.fastapi import GraphQLRouter
 from app.graphql.schema import schema
 import os
-
 from app.security import get_current_user
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(threadName)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
     seed_initial_data()
+    consumer_thread = threading.Thread(target=start_consuming)
+    consumer_thread.daemon = True
+    consumer_thread.start()
     yield
     print("Apagando la aplicación...")
 
