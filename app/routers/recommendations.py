d5bdb0b45a0aab5acb8ea957a799acfd143c57c3
Sebexe <sebyex304@gmail.com>
2025-10-12 22:08:33 -0300

    Agregada la validacion del token con la llave publica del modulo de usuarios con la dependencia jose

--- app/routers/recommendations.py
+++ app/routers/recommendations.py
@@ -5,15 +5,16 @@ from sqlmodel import Session
 
 from app.db.utils import get_session
 from app.models import RecommendationResponse
+from app.security import get_current_user, TokenPayload
 from app.services import Recommendations
 
 router = APIRouter(prefix="/api/v1/recommendations", tags=["recommendations"])
 
 
-@router.get("/content/{user_id}", response_model=List[RecommendationResponse])
-async def get_content_recommendations(user_id: int,db_session: Session = Depends(get_session)):
+@router.get("/content", response_model=List[RecommendationResponse])
+async def get_content_recommendations(db_session: Session = Depends(get_session),current_user: TokenPayload = Depends(get_current_user)):
     engine = Recommendations(db_session)
-    recommendations = engine.get_recommendations(user_id)
+    recommendations = engine.get_recommendations(current_user.user_id)
     response = [
         RecommendationResponse(movie=movie, score=score)
         for movie, score in recommendations
@@ -21,10 +22,10 @@ async def get_content_recommendations(user_id: int,db_session: Session = Depends
     return response
 
 
-@router.get("/collaborative/{user_id}", response_model=List[RecommendationResponse])
-async def get_collaborative_recommendations(user_id: int, db_session: Session = Depends(get_session)):
+@router.get("/collaborative", response_model=List[RecommendationResponse])
+async def get_collaborative_recommendations(db_session: Session = Depends(get_session),current_user: TokenPayload = Depends(get_current_user)):
     engine = Recommendations(db_session)
-    recommendations = engine.get_collaborative_recommendations(user_id)
+    recommendations = engine.get_collaborative_recommendations(current_user.user_id)
 
     response = [
         RecommendationResponse(movie=item["movie"], score=item["score"])
